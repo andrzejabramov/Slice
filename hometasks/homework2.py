573a8cd10a810a1a52157066704152f8e46657df
andrejabramov <andrzejvod@gmail.com>
2024-07-03 22:46:50 +0300

    20240703_hometasks3

---
+++ hometasks/homework2.py
@@ -0,0 +1,7 @@
+quantity_hometasks = 12
+work_hour = 1.5
+name_course = 'Python'
+timing_task = work_hour / quantity_hometasks
+
+print(f"Курс: {name_course}, всего задач: {quantity_hometasks}, "
+      f"затрачено часов: {work_hour}, среднее время выполнения {timing_task}")
