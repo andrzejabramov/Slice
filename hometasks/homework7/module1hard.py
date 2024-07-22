90a2ded42dd1f9fa0d24e4fb07c337a081d9e6b7
andrejabramov <andrzejvod@gmail.com>
2024-07-22 22:55:46 +0300

    20240722_homework7

--- hometasks/homework7/module1hard.py
+++ hometasks/homework7/module1hard.py
@@ -1,8 +1,3 @@
-import re
-
-#https://python-scripts.com/sets#create-sets
-#https://www.internet-technologies.ru/articles/mnozhestva-python.html
-
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 new_grades = []
 for el in grades:
