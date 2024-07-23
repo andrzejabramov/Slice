2f6e108c6eea2cf9890f401e80f91c07546e90c3
andrejabramov <andrzejvod@gmail.com>
2024-07-23 13:42:01 +0300

    20240723_homework11

---
+++ hometasks/homework11/module_2_4.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = [2]
+not_primes = []
+
+
+for element in numbers:
+    res = False
+    if element != 1 and element != 2:
+        for el in range(2, element):
+            if element % el == 0:
+                res = True
+                break
+        if res is True:
+            not_primes.append(element)
+        else:
+            primes.append(element)
+
+print(f"простые числа: {primes}")
+print(f"составные числа: {not_primes}")
