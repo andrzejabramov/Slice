9391774a407a0edc01f6043f016206c17373b6db
andrejabramov <andrzejvod@gmail.com>
2024-07-25 11:11:04 +0300

    20240725_homework17

---
+++ hometasks/homework17/module_3_4.py
@@ -0,0 +1,11 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for element in other_words:
+        if element.upper() in root_word.upper() or root_word.upper() in element.upper():
+            same_words.append(element)
+    return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
