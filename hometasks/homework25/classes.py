3f378dd7beb1a6028560ef78da9523b3a9a461ca
andrejabramov <andrzejvod@gmail.com>
2024-07-27 10:50:39 +0300

    20240727_homework25

--- hometasks/homework25/classes.py
+++ hometasks/homework25/classes.py
@@ -1,12 +1,15 @@
 class House:
+    houses_history = []
     def __new__(cls, *args, **kwargs):
-        instance = super().__new__(cls)
-        return instance
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
 
-    def __init__(self, name, number_of_floors, house_history):
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+    def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-        self.house_history = house_history
 
     def go_to(self, new_floor):
         note = 'Такого этажа не существует'
--- hometasks/homework25/module_5_3.py
+++ hometasks/homework25/module_5_3.py
@@ -1,6 +1,12 @@
 from classes import House
 
 
-ecs_house = House()
-
-
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+del h2
+del h3
+print(House.houses_history)
