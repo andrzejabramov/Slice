f7ffaf4a6b8ecf9e0d71473a2270e1720bf0a144
andrejabramov <andrzejvod@gmail.com>
2024-09-18 23:27:29 +0300

    20240918_homework50

--- hometasks/homework50/module_10_5.py
+++ hometasks/homework50/module_10_5.py
@@ -14,18 +14,22 @@ def read_info(name):
                 all_data.append(content)
                 count += 1
             print(f'строк в файле {n}: {count}')
-    return f'записей во всех файлах: {len(all_data)}'
+    return len(all_data)
 
 
-filenames = [f'./Files/file {number}.txt' for number in range(1, 5)]
-time_start = datetime.now()
-##линейный вызов
-#print(read_info(filenames))
-##многопроцессорный
-if __name__=='__main__':
-    with multiprocessing.Pool(processes=4) as pool:
-        pool.map(read_info, filenames)
-time_end = datetime.now()
-time_res = time_end - time_start
-print(time_res)
+start = datetime.now()
+
+###линейный вызов
+# filenames = [f'./Files/file {number}.txt' for number in range(1, 5)]
+# print(f'строк во всех файлах: {read_info(filenames)}')
+
+###многопроцессорный
+# if __name__ == '__main__':
+#     filenames = [[f'./Files/file {number}.txt'] for number in range(1, 5)]
+#     with multiprocessing.Pool(processes=4) as pool:
+#         print(f'строк во всех файлах: {sum(pool.map(read_info, filenames))}')
+#         pool.close()
+#         pool.join()
+
+print(f'Время выполнения задачи: {datetime.now() - start}')
 
